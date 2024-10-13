a62a5e4e3b9937fca2d73a17b5b5849efebdb63c
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-13 21:53:43 +0300

    refactor(dao): change return type from `void` to `T` in `add` method

--- src/main/java/org/as1iva/dao/DAO.java
+++ src/main/java/org/as1iva/dao/DAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DAO<T> {
-    void add(T t) throws SQLException;
+    T add(T t) throws SQLException;
     Optional<T> getById(int id) throws SQLException;
     List<T> getAll() throws SQLException;
     void update(T t) throws SQLException;
