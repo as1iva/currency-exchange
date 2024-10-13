96a8a4007671f97271c4f2f2b5175ac4d3f91f9d
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-13 21:54:56 +0300

    feat(dao): implement `CurrencyDAO` class

---
+++ src/main/java/org/as1iva/dao/CurrencyDAO.java
@@ -0,0 +1,123 @@
+package org.as1iva.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.as1iva.models.Currency;
+import org.as1iva.util.ConnectionManager;
+
+public class CurrencyDAO implements DAO<Currency> {
+
+    private static final CurrencyDAO INSTANCE = new CurrencyDAO();
+
+    private static final String ADD_SQL = """
+            INSERT INTO Currencies(Code, FullName, Sign)
+            VALUES (?, ?, ?)
+            """;
+
+    private static final String GET_BY_ID_SQL = """
+            SELECT ID, Code, FullName, Sign
+            FROM Currencies
+            WHERE ID = ?
+            """;
+
+    private static final String GET_ALL_SQL = """
+            SELECT ID, Code, FullName, Sign
+            FROM Currencies
+            """;
+
+    private static final String UPDATE_SQL = """
+            UPDATE Currencies
+            SET Code = ?,
+                FullName = ?,
+                Sign = ?
+            WHERE ID = ?
+            """;
+
+    private CurrencyDAO() {
+    }
+
+    public static CurrencyDAO getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Currency add(Currency currency) throws SQLException {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(ADD_SQL, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, currency.getCode());
+            preparedStatement.setString(2, currency.getFullName());
+            preparedStatement.setString(3, currency.getSign());
+
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                currency.setId(generatedKeys.getInt("ID"));
+            }
+
+            return currency;
+        }
+    }
+
+    @Override
+    public Optional<Currency> getById(int id) throws SQLException {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_BY_ID_SQL)) {
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Currency currency = null;
+
+            while (resultSet.next()) {
+                currency = new Currency(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("Code"),
+                        resultSet.getString("FullName"),
+                        resultSet.getString("Sign")
+                );
+            }
+            return Optional.ofNullable(currency);
+        }
+    }
+
+    @Override
+    public List<Currency> getAll() throws SQLException {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_SQL)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Currency> currencies = new ArrayList<>();
+
+            while (resultSet.next()) {
+                currencies.add(new Currency(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("Code"),
+                        resultSet.getString("FullName"),
+                        resultSet.getString("Sign")
+                ));
+            }
+            return currencies;
+        }
+    }
+
+    @Override
+    public void update(Currency currency) throws SQLException {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+
+            preparedStatement.setString(1, currency.getCode());
+            preparedStatement.setString(2, currency.getFullName());
+            preparedStatement.setString(3, currency.getSign());
+            preparedStatement.setInt(4, currency.getId());
+
+            preparedStatement.executeUpdate();
+        }
+    }
+}
