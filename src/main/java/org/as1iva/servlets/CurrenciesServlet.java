c0b1125edcc6b67896708262f2516be36883c708
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-21 18:42:30 +0300

    feat(servlets): implement `doPost` method in `CurrenciesServlet`

--- src/main/java/org/as1iva/servlets/CurrenciesServlet.java
+++ src/main/java/org/as1iva/servlets/CurrenciesServlet.java
@@ -3,12 +3,40 @@ package org.as1iva.servlets;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dao.CurrencyDAO;
+import org.as1iva.dto.CurrencyDTO;
+import org.as1iva.services.CurrencyService;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        String name = req.getParameter("name");
+        String sign = req.getParameter("sign");
+
+        CurrencyDTO currencyDTO = new CurrencyDTO(code, name, sign);
+
+        CurrencyService currencyService = new CurrencyService(CurrencyDAO.getInstance());
+        try {
+            currencyService.add(currencyDTO);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("Success");
     }
 
     @Override
