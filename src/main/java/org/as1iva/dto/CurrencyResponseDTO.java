0712846580ee3db01b8ddd4240a815476779067c
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-24 16:57:17 +0300

    feat(dto): implement `CurrencyResponseDTO` class

---
+++ src/main/java/org/as1iva/dto/CurrencyResponseDTO.java
@@ -0,0 +1,47 @@
+package org.as1iva.dto;
+
+public class CurrencyResponseDTO {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public CurrencyResponseDTO(Integer id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
