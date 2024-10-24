1cc7fc4d48a10fd277f6ac31342f541be83bea21
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-24 16:02:57 +0300

    feat(servlets): implement `doGet` method in `CurrencyServlet`

--- src/main/java/org/as1iva/servlets/CurrencyServlet.java
+++ src/main/java/org/as1iva/servlets/CurrencyServlet.java
@@ -1,14 +1,46 @@
 package org.as1iva.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dao.CurrencyDAO;
+import org.as1iva.dto.CurrencyRequestDTO;
+import org.as1iva.dto.CurrencyResponseDTO;
+import org.as1iva.services.CurrencyService;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+
+        CurrencyRequestDTO currencyRequestDTO = new CurrencyRequestDTO(id);
+
+        CurrencyService currencyService = new CurrencyService(CurrencyDAO.getInstance());
+
+        try {
+            CurrencyResponseDTO currencyResponseDTO = currencyService.getById(currencyRequestDTO);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(currencyResponseDTO);
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(jsonResponse);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
