596dc99bd8e99935c53f52bef032c11b8c0d094b
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-27 20:21:04 +0300

    feat(dto): implement `ExchangeRateRequestDTO` class

---
+++ src/main/java/org/as1iva/dto/ExchangeRateRequestDTO.java
@@ -0,0 +1,48 @@
+package org.as1iva.dto;
+
+import java.math.BigDecimal;
+
+public class ExchangeRateRequestDTO {
+    private Integer id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    private BigDecimal rate;
+
+    public ExchangeRateRequestDTO(int baseCurrencyId, int targetCurrencyId, BigDecimal rate) {
+        this.baseCurrencyId = baseCurrencyId;
+        this.targetCurrencyId = targetCurrencyId;
+        this.rate = rate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
