fa5ac968cfe65754edd1b50e17c340c1f3c9f8cf
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-28 13:01:16 +0300

    feat(servlets): implement `doPost` method in `ExchangeRatesServlet`

--- src/main/java/org/as1iva/servlets/ExchangeRatesServlet.java
+++ src/main/java/org/as1iva/servlets/ExchangeRatesServlet.java
@@ -1,14 +1,50 @@
 package org.as1iva.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dao.CurrencyDAO;
+import org.as1iva.dao.ExchangeRateDAO;
+import org.as1iva.dto.ExchangeRateRequestDTO;
+import org.as1iva.dto.ExchangeRateResponseDTO;
+import org.as1iva.services.ExchangeRateService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int baseCurrencyId = Integer.parseInt(req.getParameter("baseCurrencyId"));
+        int targetCurrencyId = Integer.parseInt(req.getParameter("targetCurrencyId"));
+        BigDecimal rate = new BigDecimal(req.getParameter("rate"));
+
+        ExchangeRateRequestDTO exchangeRateRequestDTO = new ExchangeRateRequestDTO(baseCurrencyId, targetCurrencyId, rate);
+
+        ExchangeRateService exchangeRateService = new ExchangeRateService(ExchangeRateDAO.getInstance(), CurrencyDAO.getInstance());
+
+        try {
+            ExchangeRateResponseDTO exchangeRateResponseDTO = exchangeRateService.add(exchangeRateRequestDTO);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(exchangeRateResponseDTO);
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(jsonResponse);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
