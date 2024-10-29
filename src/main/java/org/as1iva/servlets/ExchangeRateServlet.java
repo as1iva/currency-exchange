d6a3a68b05df98298f5f81fa83f6cfcdfc3fc1fc
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-29 14:02:22 +0300

    feat(servlets): implement `doGet` method in `ExchangeRateServlet`

--- src/main/java/org/as1iva/servlets/ExchangeRateServlet.java
+++ src/main/java/org/as1iva/servlets/ExchangeRateServlet.java
@@ -1,14 +1,47 @@
 package org.as1iva.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dao.CurrencyDAO;
+import org.as1iva.dao.ExchangeRateDAO;
+import org.as1iva.dto.ExchangeRateRequestDTO;
+import org.as1iva.dto.ExchangeRateResponseDTO;
+import org.as1iva.services.ExchangeRateService;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/exchangeRate/*")
 public class ExchangeRateServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+
+        ExchangeRateRequestDTO exchangeRateRequestDTO = new ExchangeRateRequestDTO(id);
+
+        ExchangeRateService exchangeRateService = new ExchangeRateService(ExchangeRateDAO.getInstance(), CurrencyDAO.getInstance());
+
+        try {
+            ExchangeRateResponseDTO exchangeRateResponseDTO = exchangeRateService.getById(exchangeRateRequestDTO);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(exchangeRateResponseDTO);
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(jsonResponse);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
