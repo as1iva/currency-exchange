b6b07a9f3333529ebe4203de699444a505189926
Tim Fadeev <1mailforwork1@gmail.com>
2024-10-31 15:27:54 +0300

    feat(servlets): implement `doGet` method in `ExchangeServlet`

--- src/main/java/org/as1iva/servlets/ExchangeServlet.java
+++ src/main/java/org/as1iva/servlets/ExchangeServlet.java
@@ -1,14 +1,50 @@
 package org.as1iva.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dao.CurrencyDAO;
+import org.as1iva.dao.ExchangeRateDAO;
+import org.as1iva.dto.ExchangeRequestDTO;
+import org.as1iva.dto.ExchangeResponseDTO;
+import org.as1iva.services.ExchangeService;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/exchange")
 public class ExchangeServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String baseCurrencyCode = req.getParameter("from");
+        String targetCurrencyCode = req.getParameter("to");
+        Integer amount = Integer.valueOf(req.getParameter("amount"));
+
+        ExchangeRequestDTO exchangeRequestDTO = new ExchangeRequestDTO(baseCurrencyCode, targetCurrencyCode, amount);
+
+        ExchangeService exchangeService = new ExchangeService(ExchangeRateDAO.getInstance(), CurrencyDAO.getInstance());
+
+        try {
+            ExchangeResponseDTO exchangeResponseDTO = exchangeService.exchange(exchangeRequestDTO);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(exchangeResponseDTO);
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(jsonResponse);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
