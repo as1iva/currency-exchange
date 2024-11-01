3daecf4e1a620790ad7d739ad4367af947c70423
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-01 22:46:42 +0300

    feat(dao): implement `CrudDAO` interface

--- src/main/java/org/as1iva/dao/CrudDAO.java
+++ src/main/java/org/as1iva/dao/CrudDAO.java
@@ -2,11 +2,10 @@ package org.as1iva.dao;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
-public interface DAO<T> {
+public interface CrudDAO<T> {
     T add(T t) throws SQLException;
-    Optional<T> getById(Integer id) throws SQLException;
     List<T> getAll() throws SQLException;
     void update(T t) throws SQLException;
+
 }
