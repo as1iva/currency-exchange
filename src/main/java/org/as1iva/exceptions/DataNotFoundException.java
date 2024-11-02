48387bc368fc44d0150f41788c568920dadff843
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-02 16:10:18 +0300

    feat(exceptions): implement `DataNotFoundException`

---
+++ src/main/java/org/as1iva/exceptions/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package org.as1iva.exceptions;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
