4f034fc26463363dcd578c4712ebb3e3002bd1c3
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-03 12:45:08 +0300

    feat(util): implement `ErrorHandler`

---
+++ src/main/java/org/as1iva/util/ErrorHandler.java
@@ -0,0 +1,34 @@
+package org.as1iva.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ErrorHandler {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private ErrorHandler() {
+    }
+
+    public static void handle(HttpServletResponse httpServletResponse, Exception e) throws IOException {
+        Map<String, String> errorResponse = Map.of("message", e.getMessage());
+        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(getStatus(e));
+        httpServletResponse.getWriter().write(jsonResponse);
+    }
+
+    private static int getStatus(Exception e) {
+        System.out.println("Exception class: " + e.getClass().getSimpleName());
+
+        return switch (e.getClass().getSimpleName()) {
+            case "DatabaseException" -> 500;
+            case "DataExistsException" -> 409;
+            case "DataNotFoundException" -> 404;
+            case "InvalidDataException" -> 400;
+            default -> 500;
+        };
+    }
+}
