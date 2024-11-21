2d24004e3eb0cdeffc9843f7d4efa67552d05605
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-21 12:19:13 +0300

    refactor(dto): make `ExchangeResponseDTO` immutable

--- src/main/java/org/as1iva/dto/ExchangeResponseDTO.java
+++ src/main/java/org/as1iva/dto/ExchangeResponseDTO.java
@@ -2,12 +2,12 @@ package org.as1iva.dto;
 
 import java.math.BigDecimal;
 
-public class ExchangeResponseDTO {
-    private CurrencyResponseDTO baseCurrency;
-    private CurrencyResponseDTO targetCurrency;
-    private BigDecimal rate;
-    private Integer amount;
-    private BigDecimal convertedAmount;
+public final class ExchangeResponseDTO {
+    private final CurrencyResponseDTO baseCurrency;
+    private final CurrencyResponseDTO targetCurrency;
+    private final BigDecimal rate;
+    private final Integer amount;
+    private final BigDecimal convertedAmount;
 
     public ExchangeResponseDTO(CurrencyResponseDTO baseCurrency, CurrencyResponseDTO targetCurrency, BigDecimal rate, Integer amount, BigDecimal convertedAmount) {
         this.baseCurrency = baseCurrency;
@@ -21,39 +21,19 @@ public class ExchangeResponseDTO {
         return baseCurrency;
     }
 
-    public void setBaseCurrency(CurrencyResponseDTO baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
     public CurrencyResponseDTO getTargetCurrency() {
         return targetCurrency;
-    }
-
-    public void setTargetCurrency(CurrencyResponseDTO targetCurrency) {
-        this.targetCurrency = targetCurrency;
     }
 
     public BigDecimal getRate() {
         return rate;
     }
 
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
     public Integer getAmount() {
         return amount;
     }
 
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
     public BigDecimal getConvertedAmount() {
         return convertedAmount;
-    }
-
-    public void setConvertedAmount(BigDecimal convertedAmount) {
-        this.convertedAmount = convertedAmount;
     }
 }
