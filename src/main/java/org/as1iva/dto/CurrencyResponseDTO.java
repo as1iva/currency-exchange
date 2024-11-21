4e0128e9de2cdc2c05c7b3c4778b706f4348232f
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-21 12:13:02 +0300

    refactor(dto): make `CurrencyResponseDTO` immutable

--- src/main/java/org/as1iva/dto/CurrencyResponseDTO.java
+++ src/main/java/org/as1iva/dto/CurrencyResponseDTO.java
@@ -1,10 +1,10 @@
 package org.as1iva.dto;
 
-public class CurrencyResponseDTO {
-    private Integer id;
-    private String code;
-    private String name;
-    private String sign;
+public final class CurrencyResponseDTO {
+    private final Integer id;
+    private final String code;
+    private final String name;
+    private final String sign;
 
     public CurrencyResponseDTO(Integer id, String code, String name, String sign) {
         this.id = id;
@@ -17,31 +17,15 @@ public class CurrencyResponseDTO {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getCode() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSign() {
         return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
     }
 }
