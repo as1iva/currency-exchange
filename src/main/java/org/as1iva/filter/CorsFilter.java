52093df0b3a833e17c15c1bc764f18ae5605d4ae
Tim Fadeev <1mailforwork1@gmail.com>
2024-11-23 20:37:15 +0300

    feat(filter): split `GlobalServletFilter` into `EncodingFilter` and `CorsFilter`

---
+++ src/main/java/org/as1iva/filter/CorsFilter.java
@@ -0,0 +1,24 @@
+package org.as1iva.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(value = {
+        "/currencies", "/currency/*", "/exchangeRates", "/exchangeRate/*", "/exchange"
+})
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+
+        super.doFilter(req, res, filterChain);
+    }
+}
---
+++ src/main/java/org/as1iva/filter/EncodingFilter.java
@@ -0,0 +1,25 @@
+package org.as1iva.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(value = {
+        "/currencies", "/currency/*", "/exchangeRates", "/exchangeRate/*", "/exchange"
+})
+public class EncodingFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        res.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+
+        res.setContentType("application/json");
+
+        super.doFilter(req, res, chain);
+    }
+}
